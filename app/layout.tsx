73432b97ce5986dea304944e2a4f3489d8067375
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2026-01-31 19:57:27 +0000

    Update global styles for dark theme support, modify layout for consistent dark mode appearance, and adjust Sandpack component to use dark theme. Refactor tweet component styles for dark theme compatibility and add new Gmail logo image.

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
     <html
       lang="fr"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        'dark text-white bg-[#111010]',
         GeistSans.variable,
         GeistMono.variable
       )}
