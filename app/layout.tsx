06c7bdc86c748f20a8ef8d59986a1e36567e555c
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2026-01-31 16:45:51 +0000

    Wrap children in a div with minimum height for improved layout structure and responsiveness in the RootLayout component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,9 @@ export default function RootLayout({
       <body className="antialiased max-w-3xl mb-8 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
-          {children}
+          <div className="min-h-[50vh] flex-auto">
+            {children}
+          </div>
           <Footer />
           <Analytics />
           <SpeedInsights />
