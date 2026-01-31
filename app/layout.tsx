a8a1fe488df8884cfa330bb462aa61317ead7239
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2026-01-31 19:12:36 +0000

    Add .gitattributes for LF normalization, update .gitignore to include pnpm-store, remove bun.lockb, and add components.json for configuration. Update package.json and pnpm-lock.yaml with new dependencies including tailwind-merge and lucide-react. Enhance tailwind.config.ts with new theme settings and update global styles in global.css. Introduce CustomIcon component for SVG icons and utility functions in utils.ts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
       <head>
         <SandpackCSS />
       </head>
-      <body className="antialiased max-w-3xl mb-8 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-2xl mb-8 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           <div className="min-h-[50vh] flex-auto">
